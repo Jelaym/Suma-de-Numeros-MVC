bf7b9d603dd3014f931bcabb95867f3953e58e51
David Madrigal Buendía <david.mb5@hotmail.com>
2017-08-31 23:27:58 -0500

    Update sumaTo.java

--- src/java/Modelo/sumaTo.java
+++ src/java/Modelo/sumaTo.java
@@ -9,14 +9,15 @@ import java.io.Serializable;
 public class sumaTo implements Serializable{
     sumaBo sum;
     int resultado;
-    
+    /* Metodo Constructor */
     public sumaTo(){
         resultado= 0;
     }
+    //Traemos los datos
     public void sumaBO(sumaBo suma){
         sum= suma; 
     }
-    
+    //Procesa y devuelve el dato final
     public int suma(){
         resultado= sum.getNumA() + sum.getNumB();
         return resultado;
